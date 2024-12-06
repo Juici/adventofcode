7beb22808e16e5995b1bed92116ae8e604eb337b
James Whaley <juicy66173@gmail.com>
2024-12-06 16:00:26 +0000

    Improve 2015 day 4 by using rayon

--- 2015/04/src/main.rs
+++ 2015/04/src/main.rs
@@ -1,5 +1,5 @@
-use md5::digest::Output;
 use md5::{Digest, Md5};
+use rayon::iter::{IndexedParallelIterator, IntoParallelIterator, ParallelIterator};
 
 const INPUT: &str = "iwrupvqb";
 
@@ -9,59 +9,39 @@ fn main() {
 }
 
 fn part1() {
-    let hasher = Md5::new_with_prefix(INPUT);
+    let n = (1..u32::MAX).into_par_iter().by_exponential_blocks().find_first(|&n| {
+        let mut hasher = Md5::new();
 
-    let mut buf = itoa::Buffer::new();
-    let mut n = 1;
+        hasher.update(INPUT);
+        hasher.update(itoa::Buffer::new().format(n));
 
-    let mut digest = Output::<Md5>::default();
-
-    loop {
-        let mut hasher = hasher.clone();
-
-        hasher.update(buf.format(n));
-        hasher.finalize_into(&mut digest);
-
+        let digest = hasher.finalize();
         let digest = digest.as_slice();
 
-        if matches!(digest, [0, 0, 0x0..0x10, ..]) {
-            println!("part1: {n}");
-            return;
-        }
+        matches!(digest, [0, 0, 0x0..0x10, ..])
+    });
 
-        if n == i32::MAX {
-            println!("part1: not found");
-            return;
-        }
-        n += 1;
+    match n {
+        Some(n) => println!("part1: {n}"),
+        None => println!("part1: not found"),
     }
 }
 
 fn part2() {
-    let hasher = Md5::new_with_prefix(INPUT);
+    let n = (1..u32::MAX).into_par_iter().by_exponential_blocks().find_first(|&n| {
+        let mut hasher = Md5::new();
 
-    let mut buf = itoa::Buffer::new();
-    let mut n = 1;
+        hasher.update(INPUT);
+        hasher.update(itoa::Buffer::new().format(n));
 
-    let mut digest = Output::<Md5>::default();
-
-    loop {
-        let mut hasher = hasher.clone();
-
-        hasher.update(buf.format(n));
-        hasher.finalize_into(&mut digest);
-
+        let digest = hasher.finalize();
         let digest = digest.as_slice();
 
-        if matches!(digest, [0, 0, 0, ..]) {
-            println!("part2: {n}");
-            return;
-        }
+        matches!(digest, [0, 0, 0, ..])
+    });
 
-        if n == i32::MAX {
-            println!("part2: not found");
-            return;
-        }
-        n += 1;
+    match n {
+        Some(n) => println!("part2: {n}"),
+        None => println!("part2: not found"),
     }
 }
